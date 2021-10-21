43c29e8e65a4bb418b65d5ac5bc1355de1ab6d62
Paul Choi <paulchoi1227@outlook.com>
2021-10-21 16:24:34 +1100

    Added server bootstrap

--- src/server.py
+++ src/server.py
@@ -1,1 +1,23 @@
 from socket import *
+import sys
+
+if __name__ == '__main__':
+    # When command line ags are invalid
+    if len(sys.argv) != 4:
+        print("Usage: python3 server.py server_port block_duration timeout")
+        exit()
+
+    # Process command line arguments
+    port = int(sys.argv[1])
+    block_duration = float(sys.argv[2])
+    timeout = float(sys.argv[3])
+
+    # create listening socket
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.connect(('127.0.0.1', port))
+
+    print("Starting server")
+    while True:
+        break
+
+    server_socket.close()
